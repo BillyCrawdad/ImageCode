3e16e0119e7e78de26ac64182a8435409ad65503
BillyCrawdad <107357048+BillyCrawdad@users.noreply.github.com>
2023-11-30 17:52:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from PIL import Image, ImageDraw
+def create_png():
+    # Create a new image with a white background
+    image = Image.new("RGBA", (500, 500), (255, 255, 255, 0))
 
+    # Get a drawing context
+    draw = ImageDraw.Draw(image)
+
+    # Draw a white rectangle to cover the entire image
+    draw.rectangle([(0, 0), (500, 500)], fill=(255, 255, 255, 255))
+
+    # Draw a transparent rectangle (hole) in the middle
+    draw.rectangle([(50, 50), (450, 450)], fill=(0, 0, 0, 0))
+
+    # Save the image as a PNG file
+    image.save("output.png")
+
+if __name__ == "__main__":
+    create_png()
